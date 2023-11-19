9e54ebcd27eabc6115357951578083ae9e4f661c
Francis Oliver <121164562+fxrncz@users.noreply.github.com>
2023-11-19 11:47:21 +0800

    Delete First-Projects directory

--- First-Projects/_10Tenth.java
+++
@@ -1,8 +0,0 @@
-public class _10Tenth {
-    public static void main(String[] args) {
-        System.out.println("   J    A    V     V    A");
-        System.out.println("   J   A A    V   V    A A");
-        System.out.println("J  J  AaaaA    V V    AaaaA");
-        System.out.println(" JJ  A     A    V    A     A");
-    }
-}
